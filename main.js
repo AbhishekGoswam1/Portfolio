dd56dd3b6fc944d4a7f371edde3dad40ddbe9adf
AbhishekGoswam1 <goswamiabhi9729@gmail.com>
2024-11-26 18:51:38 +0530

    added web3forms to contact form

--- main.js
+++ main.js
@@ -1,69 +1,3 @@
-// const ball = document.querySelector('.ball');
-
-// let mouseX = 0;
-// let mouseY = 0;
-
-// let ballX = 0;
-// let ballY = 0;
-
-// let speed = 0.1;
-
-// // Update ball position
-// function animate() {
-// 	//Determine distance between ball and mouse
-// 	let distX = mouseX - ballX;
-// 	let distY = mouseY - ballY;
-	
-// 	// Find position of ball and some distance * speed
-// 	ballX = ballX + (distX * speed);
-// 	ballY = ballY + (distY * speed);
-	
-// 	ball.style.left = ballX + "px";
-// 	ball.style.top = ballY + "px";
-	
-// 	requestAnimationFrame(animate);
-// }
-// animate();
-
-// // Move ball with cursor
-// document.addEventListener("mousemove", function(event) {
-// 	mouseX = event.pageX;
-// 	mouseY = event.pageY;
-// });
-
-function showMessage() {
-    var input = document.getElementById("message").value;
-    alert(input);
-    document.getElementById("message").value = ""; // Clear the input field
-}
-
-//to stop the page from reloading
-let button = document.querySelector('.submit-button');
-button.addEventListener('click', function(event) {
-    event.preventDefault();
-    showMessage();
-});
-function showMessage() {
-    var username = document.getElementById("name").value;
-    if (username) {
-        var username = username;
-        var message = `${username},Thank you for reaching out to me. I will get back to you shortly.`;
-        alert(message);
-        document.getElementById("message").value = ""; // Clear the input field
-    }
-}
-// Store messages in an array
-// let messages = [];
-
-// function showMessage() {
-//     var input = document.getElementById("message").value;
-//     if (input) {
-//         messages.push(input); // Store the message
-//         alert(input);
-//         document.getElementById("message").value = ""; // Clear the input field
-//         console.log(messages); // Log the messages to the console
-//     }
-// }
 
 // function to show the side menu
 function showSideMenu() {
@@ -76,3 +10,26 @@ function hideMenu() {
     var sideMenu = document.querySelector('.side-menu');
     sideMenu.style.display = 'none';
 }
+
+
+// function showMessage() {
+//     var input = document.getElementById("message").value;
+//     alert(input);
+//     document.getElementById("message").value = ""; // Clear the input field
+// }
+
+// //to stop the page from reloading
+// let button = document.querySelector('.submit-button');
+// button.addEventListener('click', function(event) {
+//     showMessage();
+// });
+
+// function showMessage() {
+//     var username = document.getElementById("name").value;
+//     if (username) {
+//         var username = username;
+//         var message = `${username},Thank you for reaching out to me. I will get back to you shortly.`;
+//         alert(message);
+//         document.getElementById("message").value = ""; // Clear the input field
+//     }
+// }
